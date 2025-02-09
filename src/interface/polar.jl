ad764d76602e3f459fab7f8b55166e4d6c55d421
Lukas Devos <ldevos98@gmail.com>
2025-02-09 11:39:59 -0500

    Update polar docstrings

--- src/interface/polar.jl
+++ src/interface/polar.jl
@@ -12,8 +12,8 @@ end
 """
     left_polar(A; kwargs...) -> W, P
     left_polar(A, alg::AbstractAlgorithm) -> W, P
-    left_polar(A, [WP]; kwargs...) -> W, P
-    left_polar(A, [WP], alg::AbstractAlgorithm) -> W, P
+    left_polar!(A, [WP]; kwargs...) -> W, P
+    left_polar!(A, [WP], alg::AbstractAlgorithm) -> W, P
 
 Compute the full polar decomposition of the rectangular matrix `A` of size `(m, n)`
 with `m >= n`, such that `A = W * P`. Here, `W` is an isometric matrix (orthonormal columns)
@@ -31,13 +31,20 @@ See also [`right_polar(!)`](@ref right_polar).
 """
     right_polar(A; kwargs...) -> P, Wᴴ
     right_polar(A, alg::AbstractAlgorithm) -> P, Wᴴ
-    right_polar(A, [PWᴴ]; kwargs...) -> P, Wᴴ
-    right_polar(A, [PWᴴ], alg::AbstractAlgorithm) -> P, Wᴴ
+    right_polar!(A, [PWᴴ]; kwargs...) -> P, Wᴴ
+    right_polar!(A, [PWᴴ], alg::AbstractAlgorithm) -> P, Wᴴ
 
 Compute the full polar decomposition of the rectangular matrix `A` of size `(m, n)`
 with `n >= m`, such that `A = P * Wᴴ`. Here, `P` is a positive (semi)definite matrix
 of size `(m, m)`, whereas `Wᴴ` is a matrix with orthonormal rows (its adjoint is isometric)
 of size `(n, m)`.
+
+!!! note
+    The bang method `right_polar!` optionally accepts the output structure and
+    possibly destroys the input matrix `A`. Always use the return value of the function
+    as it may not always be possible to use the provided `WP` as output.
+
+See also [`left_polar(!)`](@ref left_polar).
 """
 @functiondef right_polar
 
