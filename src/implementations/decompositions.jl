3bee5cde2972297613fcd863946c0ec547d885ae
Jutho Haegeman <jutho.haegeman@ugent.be>
2025-03-07 23:03:26 +0100

    update docstrings

--- src/implementations/decompositions.jl
+++ src/implementations/decompositions.jl
@@ -16,7 +16,8 @@ Algorithm type to denote the standard LAPACK algorithm for computing the QR deco
 a matrix using Householder reflectors. The specific LAPACK function can be controlled using
 the keyword arugments, i.e.  `?geqrt` will be chosen if `blocksize > 1`. With
 `blocksize == 1`, `?geqrf` will be chosen if `pivoted == false` and `?geqp3` will be chosen
-if `pivoted == true`.
+if `pivoted == true`. The keyword `positive =true` can be used to ensure that the diagonal
+elements of `R` are non-negative.
 """
 @algdef LAPACK_HouseholderQR
 
@@ -25,8 +26,9 @@ if `pivoted == true`.
 
 Algorithm type to denote the standard LAPACK algorithm for computing the LQ decomposition of
 a matrix using Householder reflectors. The specific LAPACK function can be controlled using
-the keyword arugments, i.e.  `?gelqt` will be chosen if `blocksize > 1` or `?gelqf` will be
-chosen if `pivoted == false`.
+the keyword arugments, i.e. `?gelqt` will be chosen if `blocksize > 1` or `?gelqf` will be
+chosen if `blocksize == 1`. The keyword `positive =true` can be used to ensure that the diagonal
+elements of `L` are non-negative.
 """
 @algdef LAPACK_HouseholderLQ
 
